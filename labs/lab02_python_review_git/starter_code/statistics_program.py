1e90f81202e2139af137e7d1528025aa2898455a
poejayden60 <poejayden60@gmail.com>
2026-06-02 13:55:35 -0400

    Created working min temp calc

--- labs/lab02_python_review_git/starter_code/statistics_program.py
+++ labs/lab02_python_review_git/starter_code/statistics_program.py
@@ -48,7 +48,12 @@ def compute_average(values):
     Compute the average of a list of numbers.
     """
     # TODO: Replace this with a correct average calculation.
-    return 0.0
+    count = len(values)
+    sum = 0.0
+    for i in range(count):
+        sum = sum + values[i]
+    average = sum / count
+    return average
 
 
 
@@ -57,7 +62,11 @@ def compute_minimum(values):
     Compute the minimum value in a list of numbers.
     """
     # TODO: Replace this with a correct minimum calculation.
-    return 0.0
+    min = values[0]
+    for temperature in values:
+        if temperature < min:
+            min = temperature
+    return min
 
 
 def compute_maximum(values):
@@ -65,6 +74,7 @@ def compute_maximum(values):
     Compute the maximum value in a list of numbers.
     """
     # TODO: Replace this with a correct maximum calculation.
+
     return 0.0
 
 
