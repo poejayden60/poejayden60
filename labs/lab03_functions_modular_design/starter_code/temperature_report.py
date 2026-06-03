58c7b27e86b4c30d3820cafad18d0f6e4812b2d0
poejayden60 <poejayden60@gmail.com>
2026-06-03 14:10:50 -0400

    Completed Assignment

--- labs/lab03_functions_modular_design/starter_code/temperature_report.py
+++ labs/lab03_functions_modular_design/starter_code/temperature_report.py
@@ -7,30 +7,40 @@ def read_temperatures(values):
             line = line.strip()
             if line != "":
                 temperatures.append(int(line))
-    return(temperatures)
+    return temperatures
+
 
 def calculate_average(values):
   #a value takes the length of the temperatures and sum is getting the some of all the temperature values and adding them and average is dividing that number by how many values there are.
     a = len(values)
     sum = 0.0
-    for i in values:
+    for i in range(a):
         sum = sum + values[i]
     average = sum / a
-    return(average)
+    return average
 
 def find_maximum(values):
     #this will automatically give the highest of temperatures
     return max(values)
 
 def find_minimum(values):
-    # this will automatically give the lowest of temperatures
+    # this will automatically give the highest of temperatures
     return min(values)
 
 def count_above_threshold(values, threshold):
-    pass
+    count = 0
+    for i in values:
+        if i > threshold:
+            count = count + 1
+    return count
 
 def print_report(values):
-    pass
+    print("Temperature Report")
+    print("------------------")
+    print(f'Average temperature: {calculate_average(values)}')
+    print(f'Maximum temperature: {find_maximum(values)}')
+    print(f'Minimum temperature: {find_minimum(values)}')
+    print(f'Temperatures above 80: {count_above_threshold(values, 80)}')
 
 def main():
     temperatures = read_temperatures("../data/june_temperatures.txt")
