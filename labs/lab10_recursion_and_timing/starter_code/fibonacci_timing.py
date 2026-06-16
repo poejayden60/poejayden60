6effa43a0ef7a59ca7772953fe3b0128699cb8ae
Edward Brash <brash@jlab.org>
2026-06-16 11:39:23 -0400

    updates

--- labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
+++ labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
@@ -1,14 +1,19 @@
 import time
 
-def fib_recursive(n):
+def fib_recursive(n)
+    # TODO: write this function
     pass
 
 
 def fib_iterative(n):
+    # TODO: write this function
     pass
 
 
 def time_function(function, n):
+    # TODO: write this function - google the python time module to figure out how it works
+    # TODO: start a timer, call the appropriate function, then stop the timer
+    # TODO: return the elapsed time
     return 0
 
 def main():
@@ -27,5 +32,9 @@ def main():
             speed = float("inf")
         print(f"{n:<5} {recursive_time:.8f} seconds    {iterative_time:.8f} seconds     {speed:.1f}")
 
+    # TODO: create a plot which shows both recursive time and iterative time as a function of n
+    # TODO: label the x-axis, y-axis, and provide a title
+    # TODO: display a legend that will indicate which dataset is which
+    # TODO: make the y-axis logarithmic
 
 main()
