8877af8d45b6c76c2fb40b7f38492d12740a87f9
Edward Brash <brash@jlab.org>
2026-06-22 11:31:45 -0400

    Fix Lab 11

--- labs/lab11_searching_sorting_rootfinding/starter_code/search_and_roots.py
+++ labs/lab11_searching_sorting_rootfinding/starter_code/search_and_roots.py
@@ -1,17 +1,67 @@
 def linear_search(values, target):
-    pass
+    comparisons = 0
+    for index, value in enumerate(values):
+        #print(index, value)
+        comparisons += 1
+        if value == target:
+            return index, comparisons
+
+    return -1, comparisons
 
 
 def binary_search(values, target):
-    pass
-
+    comparisons = 0
+    index_find = 0
+    middle_value = 0
+    # implement a binary search tree algorithm
+    while len(values) > 1:
+        comparisons += 1
+        # choose midpoint
+        middle_value = values[len(values) // 2]
+        #print(middle_value)
+        if target > middle_value:
+            index_find = index_find + len(values) // 2
+            values = values[len(values) // 2:]
+            #print("Values: ", values)
+            #print("greater ", index_find)
+        elif target < middle_value:
+            values = values[:len(values) // 2]
+            #print("Values: ", values)
+            #print("less ", index_find)
+        elif target == middle_value:
+            index_find = index_find + len(values) // 2
+            #print("Values: ", values)
+            #print("equal ", index_find)
+            return index_find, comparisons
+    if middle_value == target:
+        return index_find, comparisons
+    else:
+        return -1, comparisons
 
 def f(x):
     return x * x - 2
 
-
 def bisection_root(function, left, right, tolerance):
-    pass
+    epsilon = tolerance # tolerance for finding the root
+    fa = function(left)
+    fb = function(right)
+    #print(left, right, fa, fb)
+    if abs(fa-fb) < epsilon:
+        #print("Type 1: Found root at x = ", left) # ending condition!!!!!!!!
+        root = left
+    else:
+        c = (left+right)/2.0
+        fc = function(c)
+        if fc == 0.0:
+            #print("Type 2: Found root at x = ", c) # second ending condition!!!!
+            root = c
+        else:
+            if fa*fc < 0.0:
+                root = bisection_root(function, left, c, tolerance) # recursive call ... first half
+            else:
+                root = bisection_root(function, c, right, tolerance) # recursive call ... second half
+
+    return root
 
 
 def main():
@@ -19,8 +69,8 @@ def main():
     values = random.sample(range(0, 1000), 500)
     values.sort()
 
-    # Find the 500th value in the list
-    search_value = values[499]
+    # Find the 250th value in the list
+    search_value = values[249]
 
     print("Search Tests")
     print("------------")
@@ -32,5 +82,6 @@ def main():
     print("------------")
     root = bisection_root(f, 1, 2, 0.0001)
     print("Approximate root of x^2 - 2:", root)
+
 
 main()
